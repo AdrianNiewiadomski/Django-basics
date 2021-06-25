05d155de5a99ab0fc62ffc5e546d9b9b56baa2fc
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-06-25 13:46:55 +0200

    A lesson about passing data in url has been added.

--- 2_passing_data_in_url/passing_data_in_url/adder/urls.py
+++ 2_passing_data_in_url/passing_data_in_url/adder/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # To pass arguments from an URL to function use angle brackets.
     path("<item1>/<item2>", views.add_items)
 ]
--- 2_passing_data_in_url/passing_data_in_url/adder/views.py
+++ 2_passing_data_in_url/passing_data_in_url/adder/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.http import HttpResponse, HttpResponseBadRequest
 
 
+# Now simply add arguments mentioned in urls.py as arguments of the view function.
 def add_items(request, item1, item2):
-    result = item1 + " + " + item2 + " = " + str(int(item1) + int(item2))
-    return HttpResponse(result)
+    try:
+        result = item1 + " + " + item2 + " = " + str(float(item1) + float(item2))
+        # If the entered values are not numeric, the exception will be raised.
+
+        return HttpResponse(result)
+    except ValueError:
+        # Instead of an ordinary HttpResponse, you may use an error response with the appropriate message.
+        return HttpResponseBadRequest("An invalid number has been entered.")
